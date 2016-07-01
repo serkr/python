2ee622d4ff5efeb7760ef3d85573a0d2ed435421
serkr <sgamdev@gmail.com>
2016-07-01 18:53:17 +0300

    Right answer

--- projecteuler/1.py
+++ projecteuler/1.py
@@ -1,6 +1,12 @@
 
-number = raw_input('Insert number')
+number = raw_input('Insert number: ')
+summ = 0
 
-for n in number:
-	if n
-print number
+for n in range(int(number)):
+	print n
+	if n % 3 == 0 or n % 5 == 0: # if there will be more than 2 "or"
+		summ = summ + n
+
+print summ
+
+
